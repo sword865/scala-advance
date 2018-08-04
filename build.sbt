babbe51352d624ece05728dcc3493b92c06af808
tianhaowei <tianhaowei@yit.com>
2018-08-04 16:58:06 +0800

    printable

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "scala-advance"
 
 version := "0.1"
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.12.6"
 
-libraryDependencies += "org.typelevel" %% "cats-core" % "1.0.1"
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.2.0"
--- src/main/scala/com/advance/fps/raw/Printable.scala
+++ src/main/scala/com/advance/fps/raw/Printable.scala
@@ -4,16 +4,24 @@ trait Printable[A] {
   def format(value: A): String
 }
 
-object PrintableInstances {
-  implicit val intPrintable: Printable[Int] {
-  } = new Printable[Int] {
-    override def format(value: Int): String = value.toString
-  }
+final case class Cat(name: String, age: Int, color: String)
 
-  implicit val stringPrintable: Printable[String] {
-  } = new Printable[String] {
-    override def format(value: String): String = value
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit p: Printable[A]): String =
+      p.format(value)
+    def print(implicit p: Printable[A]): Unit =
+      println(p.format(value))
   }
+}
+
+object PrintableInstances {
+  implicit val intPrintable: Printable[Int] = (value: Int) => value.toString
+
+  implicit val stringPrintable: Printable[String] = (value: String) => value
+
+  implicit val catPrintable: Printable[Cat] = (cat: Cat) => f"${cat.name} is a ${cat.age} year-old ${cat.color} cat"
+
 }
 
 object Printable {
@@ -23,6 +31,8 @@ object Printable {
 
   def main(args: Array[String]): Unit = {
     import PrintableInstances._
-    print(1)
+    import PrintableSyntax._
+    print(Cat("kitty", 10, "white"))
+    Cat("kitty", 10, "white").print
   }
 }
